b3664b108e6be0a8450ed826868d8b45444a840a
hexidian <leokastenberg@gmail.com>
2018-04-29 09:03:44 -0400

    finish base game

--- Main.java
+++ Main.java
@@ -1,22 +1,146 @@
 import java.util.Random;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.TimeUnit;
 
 public class Main{
-  public static void main(String []args) {
-    Board board = new Board(11,5,5);
-    board.print();
+
+  int rows = 10;
+  int cols = 10;
+  Board board = new Board(20, rows, cols);
+
+  private Frame window;
+  private Label header;
+  private Panel[] grid = new Panel[rows];
+  private Button[][] button_array;
+
+  boolean mode = true;
+
+  public Main(){
+    prepareGUI();
+    fillGrid();
   }
+
+  public static void main(String []args) {
+    Main main = new Main();
+  }
+
+  private void prepareGUI(){
+    window = new Frame("Minesweeper");
+    window.setSize(50*cols,50*rows+100);
+    window.setLayout(new GridLayout(rows+2,cols));
+    window.addWindowListener(new WindowAdapter(){
+      public void windowClosing(WindowEvent windowEvent){
+        System.exit(0);
+      }
+    });
+    header = new Label();
+    header.setAlignment(Label.CENTER);
+    header.setText("Minesweeper");
+
+    for (int i  = 0; i < rows; i++){
+      grid[i] = new Panel();
+      grid[i].setLayout(new GridLayout());
+      grid[i].setSize(50*cols,50);
+    }
+    window.add(header);
+
+    Panel options = new Panel();
+    options.setLayout(new FlowLayout());
+
+    Button guess_mode = new Button("Guess Mode");
+    guess_mode.setActionCommand("guess");
+    guess_mode.addActionListener(new ModeSwitchListener());
+    options.add(guess_mode);
+
+    Button mark_mode = new Button("Mark Mode");
+    mark_mode.setActionCommand("mark");
+    mark_mode.addActionListener(new ModeSwitchListener());
+    options.add(mark_mode);
+
+    window.add(options);
+    for (int i = 0; i < rows; i++){
+      window.add(grid[i]);
+    }
+    window.setVisible(true);
+  }
+
+  //fills the grid with the grid in the board
+  private void fillGrid(){
+
+    //first we need to empty out the grid
+    for (int i = 0; i < rows; i++){
+      grid[i].removeAll();
+    }
+
+    //now we fill it with new buttons
+
+    button_array = new Button[rows][cols];//to store all the buttons
+
+    for (int i = 0; i < rows; i++){
+      for (int j = 0; j < cols; j++){
+        button_array[i][j] = new Button(board.pub_grid[i][j]);
+        button_array[i][j].setActionCommand(Integer.toString(i)+","+Integer.toString(j));
+        button_array[i][j].addActionListener(new ButtonClickListener());
+        button_array[i][j].setSize(25,25);
+        grid[i].add(button_array[i][j]);
+      }
+    }
+    window.setVisible(true);
+  }
+
+  private void changeTile(int row, int col){
+
+    grid[row].removeAll();
+
+    for (int j = 0; j < cols; j++){
+      button_array[row][j] = new Button(board.pub_grid[row][j]);
+      button_array[row][j].setActionCommand(Integer.toString(row)+","+Integer.toString(j));
+      button_array[row][j].addActionListener(new ButtonClickListener());
+      button_array[row][j].setSize(25,25);
+      grid[row].add(button_array[row][j]);
+    }
+
+    window.setVisible(true);
+
+  }
+
+  private class ButtonClickListener implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+      if(mode){
+        if (board.takeGuess(e.getActionCommand())){
+          fillGrid();
+        } else {
+          changeTile(Integer.parseInt(e.getActionCommand().split(",")[0]),Integer.parseInt(e.getActionCommand().split(",")[1]));
+        }
+      } else {
+        board.takeMark(e.getActionCommand());
+        changeTile(Integer.parseInt(e.getActionCommand().split(",")[0]),Integer.parseInt(e.getActionCommand().split(",")[1]));
+      }
+    }
+  }
+
+  private class ModeSwitchListener implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+      mode = (e.getActionCommand()=="guess");//when you switch mode
+    }
+  }
+
 }
 
 class Board {
   int mine_count;
-  int found_mines = 0;
+  int tiles_guessed = 0;
   int rows;
   int cols;
+  String[][] pub_grid;
+  Boolean[][] real_grid;
+  boolean won = false;
   public Board(int mines, int h, int w){
     //mine_count = mines;
     rows = h;
     cols = w;
-    boolean[][] real_grid = new boolean[rows][cols];
+    real_grid = new Boolean[rows][cols];
     //now we fill the real_grid with 0s
     for (int i = 0; i < rows; i++){
       for (int j = 0; j < cols; j++){
@@ -39,18 +163,83 @@ class Board {
         }
       }// end while
       real_grid[row][col] = true;
+      mine_count++;
     }// end for
 
-    String[][] pub_grid = new String[rows][cols];
-    //now we need to fill it in with @ symbols which represent unguessed tiles
+    pub_grid = new String[rows][cols];
+    //now we need to fill it in with a space which represent unguessed tiles
     for (int i = 0; i < rows; i++){
       for (int j = 0; j < cols; j++){
-        pub_grid[i][j] = "@";
+        pub_grid[i][j] = " ";
       }
     }
 
   }
+
   public void print(){
-    System.out.println(mine_count);
+
   }
+
+  public boolean takeGuess(String raw_guess){
+    String[] raw_guess_coords = raw_guess.split(",");
+    int row = Integer.parseInt(raw_guess_coords[0]);
+    int col = Integer.parseInt(raw_guess_coords[1]);
+
+    if (! (pub_grid[row][col] == " " || pub_grid[row][col] == "#")){
+      return false;//this is in case you click a tile that is already guessed
+    } else {
+      tiles_guessed++;
+      if (tiles_guessed == rows * cols - mine_count){
+        won = true;
+      }
+    }
+
+    if (real_grid[row][col]){
+      pub_grid[row][col] = "@";
+      return true;
+    } else {
+      pub_grid[row][col] = Integer.toString(nearTiles(row,col));
+      if (nearTiles(row,col)==0){
+        for (int i = -1; i < 2; i++){
+          for (int j = -1; j < 2; j++){
+            try{
+              if (pub_grid[row+i][col+j] == " " || pub_grid[row+i][col+j] == "#"){//if it hasn't been guessed yet
+                takeGuess(Integer.toString(row+i)+","+Integer.toString(col+j));
+              }
+            } catch (IndexOutOfBoundsException e) {}//do nothing
+          }
+        }
+        return true;
+      }
+      return false;
+    }
+
+  }
+
+  public void takeMark(String raw_mark){
+    String[] raw_mark_coords = raw_mark.split(",");
+    int row = Integer.parseInt(raw_mark_coords[0]);
+    int col = Integer.parseInt(raw_mark_coords[1]);
+
+    if (pub_grid[row][col]=="#"){
+      pub_grid[row][col] = " ";
+    } else {
+      pub_grid[row][col] = "#";
+    }
+
+  }
+
+  public int nearTiles(int row, int col){
+    int near = 0;
+
+    for (int i = row - 1; i < row + 2; i ++){
+      for (int j = col - 1; j < col + 2; j++){
+        try {if(real_grid[i][j]){near++;}} catch(IndexOutOfBoundsException e){}
+      }
+    }
+
+    return near;
+
+  }
+
 }
